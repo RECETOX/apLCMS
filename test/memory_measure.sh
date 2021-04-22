c2191017f3c7968e1bd49b0687573e751397752b
Ales Krenek <ljocha@ics.muni.cz>
2021-04-22 15:27:54 +0200

    first try

---
+++ test/memory_measure.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+maxcores=1
+out=
+
+while getopts o:c: opt; do case $opt in
+	c) maxcores=$OPTARG ;;
+	o) out=$OPTARG ;;
+	*) cat - >&2 <<EOF
+usage: $0 options files...
+
+-c cores	max. number of cores
+-o prefix	where to write output
+EOF
+	exit 1 ;;
+esac; done
+
+shift $(($OPTIND - 1))
+
+[ -z "$out" ] && { echo -- -o is mandatory >&2; exit 1; }
+
+files=("$@")
+
+numf=${#files[@]}
+[ $numf -lt 4 ] && { echo At least 4 files are required >&2; exit 1; }
+
+nf=(4 6 8 10 15 20 25 30)
+n=40
+while [ $n -le 100 ]; do
+	nf+=($n)
+	n=$((n+10))
+done
+
+nc=(1 2 4 8 12 16 20)
+n=24
+while [ $n -le 64 ]; do
+	nc+=($n)
+	n=$((n+8))
+done
+
+echo -n '	' >$out-ret.tsv
+echo ${nc[@]} | tr ' ' '	' >>$out-ret.tsv
+
+echo -n '	' >$out-mem.tsv
+echo ${nc[@]} | tr ' ' '	' >>$out-mem.tsv
+
+echo -n '	' >$out-time.tsv
+echo ${nc[@]} | tr ' ' '	' >>$out-time.tsv
+
+
+for f in ${nf[@]}; do
+	[ $f -gt $numf ] && break
+
+	echo -n "$f	" >>$out-ret.tsv
+	echo -n "$f	" >>$out-mem.tsv
+	echo -n "$f	" >>$out-time.tsv
+
+	for c in ${nc[@]}; do
+		if [ $c -lt  $((f / 4)) -o $c -gt $maxcores -o $c -gt $f ]; then
+			echo -n 'nan	' >>$out-ret.tsv
+			echo -n 'nan	' >>$out-mem.tsv
+			echo -n 'nan	' >>$out-time.tsv
+			continue
+		fi
+		echo $(dirname $0)/memory_single_run.sh -c $c -o $out-${f}_$c.out -e $out-${f}_$c.err ${files[@]:0:$f}
+		res=(-1 -2 -3)
+		res=($($(dirname $0)/memory_single_run.sh -c $c -o $out-${f}_$c.out -e $out-${f}_$c.err ${files[@]:0:$f}))
+		echo -n "${res[0]}	" >>$out-ret.tsv
+		echo -n "${res[1]}	" >>$out-mem.tsv
+		echo -n "${res[2]}	" >>$out-time.tsv
+	done
+	echo >>$out-ret.tsv
+	echo >>$out-mem.tsv
+	echo >>$out-time.tsv
+done
---
+++ test/memory_single_run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+cores=1
+out=
+err=
+
+default_image=recetox/aplcms:6.6.6-recetox2
+image=$default_image
+
+while getopts c:o:e:i: opt; do case $opt in
+	c) cores=$OPTARG;;
+	o) out=">$OPTARG";;
+	e) err="2>$OPTARG";;
+	i) image=$OPTARG;;
+	*) cat >&2 <<EOF
+usage: $0 options input_files...
+
+print apLCMS exit code (\$?), memory usage (bytes) and time to execute (seconds)
+
+-c cores 	how many cores to use (default 1)
+-o file		redirect apLCMS stdout to file
+-e file		redirect apLCMS stderr to file
+-i image	docker image to use (default $default_image)
+
+input paths must reside in the current working directory 
+
+EOF
+exit 1
+	;;
+esac; done
+
+shift $(($OPTIND - 1))
+
+files="'$1'"
+shift
+for f in "$@"; do
+	files="$files,'$f'"
+done
+
+id=$(docker run -u $(id -u) -d -v $PWD:/work -w /work $image sleep 9876543210)
+
+start=$(date +%s)
+docker exec -u $(id -u) -ti $id bash -c "Rscript -e \"x <- apLCMS::unsupervised(files=c($files),min_exp = 2, min_pres = 0.5, min_run = 12.0, mz_tol = 1e-05, baseline_correct = 0.0, baseline_correct_noise_percentile = 0.05, intensity_weighted = FALSE, shape_model = 'bi-Gaussian', BIC_factor = 2.0, peak_estim_method = 'moment', min_bandwidth = , max_bandwidth = , sd_cut = c(0.01, 500.0), sigma_ratio_lim = c(0.01, 100.0), component_eliminate = 0.01, moment_power = 1.0, align_chr_tol = , align_mz_tol = , max_align_mz_diff = 0.01, recover_mz_range = , recover_chr_range = , use_observed_range = TRUE, recover_min_count = 3, cluster = as.integer('$cores') )\"" $out $err
+ret=$?
+end=$(date +%s)
+time=$(($end - $start))
+mem=$(cat /sys/fs/cgroup/memory/docker/$id/memory.max_usage_in_bytes)
+
+docker rm -f $id >/dev/null
+
+echo $ret $mem $time
